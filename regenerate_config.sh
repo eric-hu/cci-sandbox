e75bfbcc8394022e2303e36a32ed988da2271d7b
Eric Hu <eric@circleci.com>
2018-09-04 13:08:10 +0700

    Update "config pack" example

--- regenerate_config.sh
+++ regenerate_config.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-# Need to remove the old config to make sure it doesn't get packed into the final product
-if [[ ! -f .circleci/config.yml ]]; then
-  rm .circleci/config.yml
-fi
-circleci config pack .circleci > .circleci/config.yml
+circleci config pack .circleci/template > .circleci/config.yml
